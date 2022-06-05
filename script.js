8f417284d45037e5b284588b49bdced4f956212e
Tanya <tanyapanchal1999@gmail.com>
2022-06-05 18:49:39 +0530

    added chaanges.

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.guess').value = 23;
+*/
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    console.log(
+      (document.querySelector('.message').textContent = ' 🛑 No number!')
+    );
+  } else if (guess == secretNumber) {
+    console.log(
+      (document.querySelector('.message').textContent = 'Correct Number!')
+    );
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too low !';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lost the Game!';
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
